66516fa76ca5a3814461d37e13e22feb38ca4377
root <root@DESKTOP-UE7169K>
2024-08-23 11:26:14 +0900

    style: TodayHabitPage.css 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/global.css";
-// import Main from "./main";
+import Main from "./main";
 import HomePage from "./components/pages/HomePage/HomePage";
 import CreateStudyPage from "./components/pages/CreateStudyPage/CreateStudyPage";
 import StudyPage from "./components/pages/StudyPage/StudyPage";
@@ -9,36 +9,36 @@ import TodatHabitPage from "./components/pages/TodayHabitPage/TodayHabitPage";
 import TodaysFocusPage from "./components/pages/TodaysFocusPage/TodaysFocusPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="create" element={<CreateStudyPage />} />
-        <Route path="study/:studyId" element={<StudyPage />} />
-        <Route path="study/:studyId/todayHabit" element={<TodatHabitPage />} />
-        <Route
-          path="study/:studyId/todaysFocus"
-          element={<TodaysFocusPage />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
-
   // return (
   //   <BrowserRouter>
   //     <Routes>
-  //       <Route path="/" element={<Main />}>
-  //         <Route index element={<HomePage />} />
-  //         <Route path="create" element={<CreateStudyPage />} />
-  //         <Route path="study/:studyId">
-  //           <Route index element={<StudyPage />} />
-  //           <Route path="todayHabit" element={<TodatHabitPage />} />
-  //           <Route path="todaysFocus" element={<TodaysFocusPage />} />
-  //         </Route>
-  //       </Route>
+  //       <Route path="/" element={<HomePage />} />
+  //       <Route path="create" element={<CreateStudyPage />} />
+  //       <Route path="study/:studyId" element={<StudyPage />} />
+  //       <Route path="study/:studyId/todayHabit" element={<TodatHabitPage />} />
+  //       <Route
+  //         path="study/:studyId/todaysFocus"
+  //         element={<TodaysFocusPage />}
+  //       />
   //     </Routes>
   //   </BrowserRouter>
   // );
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<HomePage />} />
+          <Route path="create" element={<CreateStudyPage />} />
+          <Route path="study/:studyId">
+            <Route index element={<StudyPage />} />
+            <Route path="todayHabit" element={<TodatHabitPage />} />
+            <Route path="todaysFocus" element={<TodaysFocusPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/pages/TodayHabitPage/TodayHabitPage.js
+++ src/components/pages/TodayHabitPage/TodayHabitPage.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import CurrentTime from "./components/CurrentTime";
 import HabitsList from "./components/HabitsList";
 import ListModal from "./components/ListModal";
+import "./TodayHabitPage.css";
 
 function TodatHabitPage() {
   const [modalOn, setModalOn] = useState(false);
@@ -18,21 +19,25 @@ function TodatHabitPage() {
   };
 
   return (
-    <>
-      <CurrentTime />
-      <HabitsList
-        studyId={studyId}
-        patchList={patchListHandler}
-        pageRender={pageRender}
-        setPageRender={setPageRender}
-      />
-      <ListModal
-        studyId={studyId}
-        modalState={modalOn}
-        patchList={patchListHandler}
-        setPageRender={setPageRender}
-      />
-    </>
+    <div className="main__todayHabitPage border-box flex-row">
+      <div className="todayHabitpage__contaner border-box flex-row">
+        <div className="todayHaibtPage__contaner__content border-box ">
+          <CurrentTime />
+          <HabitsList
+            studyId={studyId}
+            patchList={patchListHandler}
+            pageRender={pageRender}
+            setPageRender={setPageRender}
+          />
+          <ListModal
+            studyId={studyId}
+            modalState={modalOn}
+            patchList={patchListHandler}
+            setPageRender={setPageRender}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
