1817c86ead94a1259b03af8496cd4c1644463e26
yoolri <lucca6042@naver.com>
2024-08-23 13:46:21 +0900

    design: 버튼 클릭 ui 변경 restart 상태 생성 버튼 활성화, 비활성화 상태 UI 수정

--- src/components/pages/TodaysFocusPage/components/Timer.js
+++ src/components/pages/TodaysFocusPage/components/Timer.js
@@ -11,18 +11,39 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
   var initPoint;
   if (initialPoint) {
     initPoint = initialPoint;
-  }
-  const [countTime, setCountTime] = useState(T);
-  const [initialTime, setInitialTime] = useState(T);
+  };
+
+  const [countTime, setCountTime] = useState(T);  // 입력한 시간 계산 후 카운트다운되는 숫자
+  const [initialTime, setInitialTime] = useState(T); //입력한 시간 계산 후 저장되는 숫자
   const [minute, setMinute] = useState(M);
   const [second, setSecond] = useState(S);
-  const [pauseTimer, setPauseTimer] = useState("");
-  const [clearTimer, setClearTimer] = useState("");
-  const [stopTimer, setStopTimer] = useState("");
-  const [alertCondition, setAlertCondition] = useState("");
-  const [totalPoint, setTotalPoint] = useState(initPoint);
-  const [timerState, setTimerState] = useState(true);
-  const [cssState, setCssState] = useState(false);
+  const [pauseTimer, setPauseTimer] = useState(""); // 일시정지 버튼 동적 할당
+  const [clearTimer, setClearTimer] = useState(""); // 초기화 버튼 동적 할당
+  const [controlTimer, setControltimer] = useState(""); // 시작, 재시작, 종료 버튼 동적 할당
+  const [alertCondition, setAlertCondition] = useState(""); // 상태 메세지 동적 할당
+  const [totalPoint, setTotalPoint] = useState(initPoint); // 획득한 포인트를 더한 총 포인트
+  const [state, setState] = useState(false); // 상태에 따라 css 클래스 변경
+  const [btnText, setBtnText] = useState('Start'); // 상태에 따라 css 클래스 변경
+
+  function changeCss(form, state) {
+    const button = {
+      false: "",
+      start: "start",
+      over: "nodisplay",
+      pause: "restart",
+    };
+
+    console.log(state)
+
+    const input = {
+      false: "",
+      start: "vividred",
+      over: "gray",
+      pause: "",
+    };
+    
+    return form === 'input' ? input[state] : button[state];
+  }
 
   // 변수 초기화
   function init() {
@@ -33,8 +54,7 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
     setClearTimer("");
     setPauseTimer("");
     setAlertCondition("");
-    setTimerState(true);
-    setCssState(false);
+    setState(false);
   }
 
   //INPUT값 받아와서 변환
@@ -51,16 +71,16 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
   };
 
 
-  var obtainPoint = 0; //획득하는 포인트 저장
-  var timer; //setInterval 저장
+  var obtainPoint = 0; // 획득하는 포인트 저장
+  var timer; // setInterval 저장
 
   //start버튼 클릭
   const StartTimer = (e) => {
-    setAlertGetPoint("");
-    setCssState(true);
-    e.target.className += "start";
-    setAlertCondition("");
     var count = countTime;
+    setAlertGetPoint("");
+    setState("start");
+    setBtnText("Start");
+    setAlertCondition("");
 
     setPauseTimer(
       <button
@@ -94,11 +114,10 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
           obtainPoint = obtainPoint + 3;
         }
       } else {
-        setCssState("over");
-        setTimerState(false);
+        setState("over");
         setClearTimer("");
         setPauseTimer("");
-        setStopTimer(
+        setControltimer(
           <button
             type="button"
             id="stoptbtn"
@@ -114,12 +133,12 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
         } else {
           setMinute(changeDigits(Math.floor(Math.abs(count) / 60)));
           setSecond(changeDigits(Math.abs(count % 60)));
-        }
+        };
 
         if (count % 600 === 0) {
           obtainPoint++;
           console.log(obtainPoint);
-        }
+        };
       }
     }, 1000);
   };
@@ -130,7 +149,7 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
     setTotalPoint(total);
     init();
     clearInterval(timer);
-    setStopTimer("");
+    setControltimer("");
     setPoint(total);
     setAlertGetPoint(
       <div className="condition__alert points font16">
@@ -145,6 +164,8 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
 
   //pause버튼 클릭
   const PauseTimer = () => {
+    setBtnText("ReStart");
+    setState("pause");
     setAlertCondition(
       <div className="condition__alert pause font16">
         {" "}
@@ -180,22 +201,19 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
         {changeTime(initialTime)}
       </div>
       <div
-        className={`timer font150 ${cssState ? (cssState === "over" ? "gray" : "vividred") : ""
-          }`}
+        className={`timer font150 ${changeCss('input', state)}`}
       >
-        <span>{timerState ? "" : "-"}</span>
+        <span>{state === "over" ? "-" : ""}</span>
         <input
           type="text"
-          className={`timer__input font150 ${cssState ? (cssState === "over" ? "gray" : "vividred") : ""
-            }`}
+          className={`timer__input font150 ${changeCss('input', state)}`}
           value={minute}
           onChange={timerMinute}
         />{" "}
         <span>:</span>
         <input
           type="text"
-          className={`timer__input font150 ${cssState ? (cssState === "over" ? "gray" : "vividred") : ""
-            }`}
+          className={`timer__input font150 ${changeCss('input', state)}`}
           value={second}
           onChange={timerSecond}
         />
@@ -203,19 +221,19 @@ const Timer = ({ initialPoint, setPoint, setAlertGetPoint, id }) => {
 
       <div className="timer__controls">
         {pauseTimer}
-        {stopTimer}
+        {controlTimer}
         <button
           type="button"
           id="startbtn"
-          className={`timer__controls__active ${timerState ? "" : "nodisplay"}`}
+          className={`timer__controls__active ${changeCss('button', state)}`}
           onClick={StartTimer}
         >
           {" "}
-          &nbsp;&nbsp; Start!{" "}
+          &nbsp;&nbsp; {btnText}!{" "}
         </button>
         {clearTimer}
       </div>
-      <div className="condition">{alertCondition}</div>
+      <div className="content__alert">{alertCondition}</div>
     </>
   );
 };
