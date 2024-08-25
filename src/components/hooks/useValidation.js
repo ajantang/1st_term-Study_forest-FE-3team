b625f56ece9ff6821a9f024671566fd514555fb0
wxy0415 <qkrtkd727@gmail.com>
2024-08-26 00:39:21 +0900

    feat: 유효성검사의 조건과 문구들 추가됐습니다

--- src/components/hooks/useValidation.js
+++ src/components/hooks/useValidation.js
@@ -10,34 +10,40 @@ const useValidation = () => {
 
   const validateNickname = () => {
     if (nickname === '') return null;
-    if (nickname.length < 2) return '2자 이상 입력해주세요';
-    if (nickname.length > 12) return '12자 이내로 입력해주세요';
+    if (nickname.length < 2) return '*2자 이상 입력해주세요';
+    if (nickname.length > 12) return '*12자 이내로 입력해주세요';
+    if (nickname.includes(' ')) return '*공백은 허용하지 않습니다';
     return '';
   };
 
   const validateStudyName = () => {
+    const trimmedStudyName = studyName.trim();
     if (studyName === '') return null;
-    if (studyName.length < 2) return '2자 이상 입력해주세요';
-    if (studyName.length > 20) return '20자 이내로 입력해주세요';
+    if (trimmedStudyName === '') return '*스터디 이름을 입력해주세요';
+    if (trimmedStudyName.length < 2) return '*2자 이상 입력해주세요';
+    if (trimmedStudyName.length > 20) return '*20자 이내로 입력해주세요';
+
     return '';
   };
 
   const validateDescription = () => {
     if (description === '') return null;
-    if (description.length > 100) return '100자 이내로 입력해주세요';
+    if (description.length > 100) return '*100자 이내로 입력해주세요';
     return '';
   };
 
   const validatePassword = () => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+[\]{};':"\\|,.<>/?`~]{8,}$/; //영문자와 숫자를 포함하고 특수문자 여부는 상관없음
     if (password === '') return null;
-    if (password.length < 8) return '영문, 숫자를 포함하여 8자 이상 입력해주세요';
-    if (password.length > 24) return '24자 이내로 입력해주세요';
+    if (password.includes(' ')) return '*공백은 허용하지 않습니다';
+    if (!passwordRegex.test(password)) return '*영문, 숫자를 포함하여 8자 이상 입력해주세요';
+    if (password.length > 24) return '*24자 이내로 입력해주세요';
     return '';
   };
 
   const validateConfirmPassword = () => {
     if (confirmPassword === '') return null;
-    if (password !== confirmPassword || confirmPassword.length < 8) return '비밀번호가 일치하지 않습니다';
+    if (password !== confirmPassword || confirmPassword.length < 8) return '*비밀번호가 일치하지 않습니다';
     return '';
   };
 
