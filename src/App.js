5c57735a001ffff56678e3da39be08aca05f48a8
yoorli <lucca6042@naver.com>
2024-08-28 17:54:58 +0900

    fix:경로 오류 수정
    일단 원래 코드는 주석처리
    Main, privateRoute 연결 수정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/global.css";
 import Main from "./main";
-// import HomePage from "./components/pages/HomePage/HomePage";
-// import CreateStudyPage from "./components/pages/CreateStudyPage/CreateStudyPage";
+import HomePage from "./components/pages/HomePage/HomePage";
+import CreateStudyPage from "./components/pages/CreateStudyPage/CreateStudyPage";
 import StudyPage from "./components/pages/StudyPage/StudyPage";
 import TodatHabitPage from "./components/pages/TodayHabitPage/TodayHabitPage";
 import TodaysFocusPage from "./components/pages/TodaysFocusPage/TodaysFocusPage";
@@ -12,8 +12,8 @@ import PrivateRoute from "../src/utils/PrivateRoute";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<p>home</p>}>
+      <Routes> 
+        {/* <Route path="/" element={<p>home</p>}>
           <Route path="create" element={<CreateStudyPage />} />
           <Route path="study/:studyId" element={<StudyPage />} />
           <Route element={<PrivateRoute />}>
@@ -26,19 +26,22 @@ function App() {
               element={<TodaysFocusPage />}
             />
           </Route>
-        </Route>
-        {/* <Route path="/" element={<HomePage />}>
-          <Route path="create" element={<CreateStudyPage />} />
-          <Route path="study/:studyId" element={<StudyPage />} />
-          <Route
-            path="study/:studyId/todayHabit"
-            element={<TodatHabitPage />}
-          />
-          <Route
-            path="study/:studyId/todaysFocus"
-            element={<TodaysFocusPage />}
-          />
         </Route> */}
+        <Route element={<Main />}>
+          <Route path="/" element={<HomePage />} />
+            <Route path="create" element={<CreateStudyPage />} />
+            <Route path="study/:studyId" element={<StudyPage />} />
+            <Route element={<PrivateRoute />}>
+              <Route
+                path="study/:studyId/todayHabit"
+                element={<TodatHabitPage />}
+              />
+              <Route
+                path="study/:studyId/todaysFocus"
+                element={<TodaysFocusPage />}
+              />
+            </Route>
+          </Route>
       </Routes>
     </BrowserRouter>
   );
