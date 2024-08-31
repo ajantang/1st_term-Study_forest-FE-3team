35db4e527e4ac92e3bcf65692748d1185660bad2
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-08-31 09:15:49 +0900

    style : a tag div로 변경

--- src/components/UI/Loading.js
+++ src/components/UI/Loading.js
@@ -2,9 +2,9 @@ import "./Loading.css";
 
 export function Loading() {
   return (
-    <a className="loading-top">
+    <div className="loading-top">
       <img className="spinning-dot" alt="로딩 화면" />
-    </a>
+    </div>
   );
 }
 
